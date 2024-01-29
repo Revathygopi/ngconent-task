2bb86fcb990140c80f0986f6b8de44728b4c61c6
revathy <revathy@gmail.com>
2024-01-29 10:52:09 +0530

    task completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NgContentComponent } from './ng-content/ng-content.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgContentComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ngconent-task';
+ 
+
 }
