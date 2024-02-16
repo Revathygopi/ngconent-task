b84a13938f0eb972d5257e85c54885eeda3d6cbf
revathy <revathy@gmail.com>
2024-02-16 10:32:55 +0530

    used form builder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgContentComponent } from './ng-content/ng-content.component';
 import {
@@ -6,7 +6,7 @@ import {
   FormGroup,
   ReactiveFormsModule,
   Validators,
-  FormArray,
+  FormArray,FormBuilder ,
 } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 @Component({
@@ -22,31 +22,47 @@ import { CommonModule } from '@angular/common';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  login = new FormGroup({
-    fname: new FormControl('', [Validators.required]),
-    lname: new FormControl('', [Validators.required]),
-    email: new FormControl('',[Validators.required, Validators.email]),
-    password: new FormControl('',[Validators.required, Validators.minLength(7),Validators.pattern('[a-z]+')]),
-    passwordvisible:new FormControl(''),
-    dob: new FormControl(''),
-    add: new FormArray([
-      new FormGroup(
-        {
-          phno:new FormControl('',[Validators.required]),
-          address:new FormControl('')
+  formbuilder=inject(FormBuilder);
+ 
+  login=this.formbuilder.group({
+    fname:['', [Validators.required]],
+    lname:['', [Validators.required]],
+    email:['',[Validators.required, Validators.email]],
+    password:['',[Validators.required, Validators.minLength(7),Validators.pattern('[a-z]+')]],
+    passwordvisible:['', [Validators.required]],
+    dob:['', [Validators.required]],
+  address:this.formbuilder.array([
+    this.formbuilder.group({
+      address:['', [Validators.required]],
+      phone:['', [Validators.required]],
+    })
+  ])
+  });
+  // login = new FormGroup({
+  //   fname: new FormControl('', [Validators.required]),
+  //   lname: new FormControl('', [Validators.required]),
+  //   email: new FormControl('',[Validators.required, Validators.email]),
+  //   password: new FormControl('',[Validators.required, Validators.minLength(7),Validators.pattern('[a-z]+')]),
+  //   passwordvisible:new FormControl(''),
+  //   dob: new FormControl(''),
+  //   add: new FormArray([
+  //     new FormGroup(
+  //       {
+  //         phno:new FormControl('',[Validators.required]),
+  //         address:new FormControl('')
 
-        }
-      )])
+  //       }
+  //     )])
      
 
-  });
+  // });
 
   getAddressFromArray() {
-    return this.login.get('add') as FormArray;
+    return this.login.get('address') as FormArray;
   }
 
   controltoAddress() {
-    this.getAddressFromArray().push(new FormControl('add'));
+    this.getAddressFromArray().push(new FormControl('address'));
   }
   removeAdress(i: number) {
     this.getAddressFromArray().removeAt(i);
